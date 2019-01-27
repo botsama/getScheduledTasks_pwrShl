9c8cbd191da8861c050b1b621f1fade07b9fc28c
Pic0o <47042955+botsama@users.noreply.github.com>
2019-01-27 06:11:30 -0500

    Add files via upload

---
+++ execTasksSch.cmd
@@ -0,0 +1,2 @@
+@echo off
+PowerShell.exe -ExecutionPolicy Bypass -File getSchTasks.ps1
---
+++ getSchTasks.ps1
@@ -0,0 +1,10 @@
+﻿# Collect Event Logs from Local Machine
+# This should be run as Administrator from escalated level console to get all Scripts.
+
+$boxName = $env:COMPUTERNAME
+$outSchTsk01 = ".\$($boxName)_Scheduled-Tasks.log"
+
+Filter timestamp {"Logs collected at $(Get-Date -Format "yyyy-MM-dd HH mm ss")"}
+
+Get-ScheduledTask | Select TaskName, State, TaskPath | Sort-Object -Property TaskPath | Format-table -wrap | Out-File $outSchTsk01
+timestamp | Out-File -Append $outSchTsk01
